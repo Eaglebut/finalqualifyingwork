7596357dee67cd59c72e802b390b7a42636a8944
Veniamin Tishenkov <Eaglebuttt@gmail.com>
2021-04-21 19:09:35 +0300

    Удалил лишнее

--- src/main/java/ru/sfedu/finalqualifyingwork/rest/TestController.java
+++ src/main/java/ru/sfedu/finalqualifyingwork/rest/TestController.java
@@ -1,4 +1,4 @@
-package ru.sfedu.finalqualifyingwork.controller;
+package ru.sfedu.finalqualifyingwork.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
