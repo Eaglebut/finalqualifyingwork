54c92a368f4ea35bc3d4280efdeb754026a8de11
Veniamin Tishenkov <Eaglebuttt@gmail.com>
2021-04-26 12:14:04 +0300

    Подключил сваггер

--- src/main/java/ru/sfedu/finalqualifyingwork/config/SecurityConfig.java
+++ src/main/java/ru/sfedu/finalqualifyingwork/config/SecurityConfig.java
@@ -1,27 +1,16 @@
 package ru.sfedu.finalqualifyingwork.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import ru.sfedu.finalqualifyingwork.model.enums.Permission;
-import ru.sfedu.finalqualifyingwork.model.enums.Role;
-import ru.sfedu.finalqualifyingwork.repository.HibernateUserDao;
-import ru.sfedu.finalqualifyingwork.repository.UserDao;
 import ru.sfedu.finalqualifyingwork.security.JwtConfigurer;
 
 
@@ -36,6 +25,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     this.jwtConfigurer = jwtConfigurer;
   }
 
+  private static final String[] AUTH_WHITELIST = {
+          // -- Swagger UI v2
+          "/v2/api-docs",
+          "/swagger-resources",
+          "/swagger-resources/**",
+          "/configuration/ui",
+          "/configuration/security",
+          "/swagger-ui.html",
+          "/webjars/**",
+          // -- Swagger UI v3 (OpenAPI)
+          "/v3/api-docs/**",
+          "/swagger-ui/**"
+          // other public endpoints of your API may be appended to this array
+  };
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
@@ -45,11 +49,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
             .antMatchers("/").permitAll()
             .antMatchers("/api/v1/auth/login").permitAll()
+            .antMatchers("/api/v1/auth/register").permitAll()
+            .antMatchers(AUTH_WHITELIST).permitAll()
             .anyRequest()
             .authenticated()
             .and()
             .apply(jwtConfigurer)
     ;
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/v1/api-docs",
+            "/configuration/ui",
+            "/swagger-resources/**",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**");
   }
 
   @Bean
