e1191c606a99983ccc3c174a241c78b4436ea0f0
Veniamin Tishenkov <Eaglebuttt@gmail.com>
2021-05-04 12:11:41 +0300

    Рефакторинг ошибок

--- src/main/java/ru/sfedu/finalqualifyingwork/rest/api/v1/GroupController.java
+++ src/main/java/ru/sfedu/finalqualifyingwork/rest/api/v1/GroupController.java
@@ -2,10 +2,12 @@ package ru.sfedu.finalqualifyingwork.rest.api.v1;
 
 import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import ru.sfedu.finalqualifyingwork.model.Group;
 import ru.sfedu.finalqualifyingwork.model.enums.GroupType;
 import ru.sfedu.finalqualifyingwork.model.enums.UserRole;
@@ -16,8 +18,6 @@ import ru.sfedu.finalqualifyingwork.rest.api.v1.dto.group.PostGroupDto;
 import ru.sfedu.finalqualifyingwork.security.JwtTokenProvider;
 import ru.sfedu.finalqualifyingwork.util.Statuses;
 
-import java.rmi.UnexpectedException;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RestController
@@ -30,19 +30,23 @@ public class GroupController {
   private final JwtTokenProvider jwtTokenProvider;
 
 
+  private HttpClientErrorException createHttpException(HttpStatus status, String message) {
+    return HttpClientErrorException.create(status, message, HttpHeaders.EMPTY, null, null);
+  }
+
   @GetMapping()
   @PreAuthorize("hasAuthority('user:all')")
   public ResponseEntity<?> getUsersGroups(@RequestHeader("Authorization") @ApiParam(hidden = true) String token) {
     try {
       return new ResponseEntity<>(groupDao.getGroup(userDao.getUser(jwtTokenProvider.getUsername(token))
-              .orElseThrow(() -> new NoSuchElementException("user not founded")))
+              .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "user not founded")))
               .stream()
               .filter(group -> !group.getGroupType().equals(GroupType.SUBGROUP))
               .map(GetGroupDto::new)
               .collect(Collectors.toList()),
               HttpStatus.OK);
-    } catch (NoSuchElementException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    } catch (HttpClientErrorException exception) {
+      return new ResponseEntity<>(exception.getStatusText(), exception.getStatusCode());
     }
   }
 
@@ -52,16 +56,15 @@ public class GroupController {
                                     @PathVariable long id) {
     try {
       var user = userDao.getUser(jwtTokenProvider.getUsername(token))
-              .orElseThrow(() -> new NoSuchElementException("user not founded"));
-      var group = groupDao.getGroup(id).orElseThrow(() -> new NoSuchElementException("group not founded"));
+              .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "user not founded"));
+      var group = groupDao.getGroup(id).orElseThrow(() ->
+              createHttpException(HttpStatus.NOT_FOUND, "group not founded"));
       if (!group.getMemberList().containsKey(user)) {
-        throw new IllegalAccessException("User is not a part of this group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User is not a part of this group");
       }
       return new ResponseEntity<>(new GetGroupDto(group), HttpStatus.OK);
-    } catch (NoSuchElementException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    } catch (IllegalAccessException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
+    } catch (HttpClientErrorException exception) {
+      return new ResponseEntity<>(exception.getStatusText(), exception.getStatusCode());
     }
   }
 
@@ -71,7 +74,7 @@ public class GroupController {
                                        @RequestBody PostGroupDto groupDto) {
     try {
       var user = userDao.getUser(jwtTokenProvider.getUsername(token))
-              .orElseThrow(() -> new NoSuchElementException("user not founded"));
+              .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "user not founded"));
       Group group = new Group();
       group.setName(groupDto.getName());
       group.getMemberList().put(user, UserRole.CREATOR);
@@ -79,29 +82,25 @@ public class GroupController {
       if (groupDto.getBaseGroupId() != 0) {
         group.setGroupType(GroupType.SUBGROUP);
         var baseGroup = groupDao.getGroup(groupDto.getBaseGroupId())
-                .orElseThrow(() -> new NoSuchElementException("Group not founded"));
+                .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "Group not founded"));
         if (!baseGroup.getMemberList().containsKey(user)) {
-          throw new NoSuchElementException("User is not a part of a base group");
+          throw createHttpException(HttpStatus.FORBIDDEN, "User is not a part of a base group");
         }
         if (baseGroup.getMemberList().get(user).equals(UserRole.MEMBER)) {
-          throw new IllegalAccessException("User does not have enough rights to create a subgroup");
+          throw createHttpException(HttpStatus.FORBIDDEN, "User does not have enough rights to create a subgroup");
         }
         group.setBaseGroup(baseGroup);
         if (!groupDao.createGroup(group).equals(Statuses.SUCCESS)) {
-          throw new UnexpectedException("Something goes wrong");
+          throw createHttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Something goes wrong");
         }
         return ResponseEntity.ok(new GetGroupDto(group));
       }
       if (!groupDao.createGroup(group).equals(Statuses.SUCCESS)) {
-        throw new UnexpectedException("Something goes wrong");
+        throw createHttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Something goes wrong");
       }
       return ResponseEntity.ok(new GetGroupDto(group));
-    } catch (NoSuchElementException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    } catch (UnexpectedException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    } catch (IllegalAccessException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
+    } catch (HttpClientErrorException exception) {
+      return new ResponseEntity<>(exception.getStatusText(), exception.getStatusCode());
     }
   }
 
@@ -112,23 +111,24 @@ public class GroupController {
                                        @PathVariable long id) {
     try {
       var user = userDao.getUser(jwtTokenProvider.getUsername(token))
-              .orElseThrow(() -> new NoSuchElementException("user not founded"));
-      var group = groupDao.getGroup(id).orElseThrow(() -> new NoSuchElementException("Group not founded"));
+              .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "user not founded"));
+      var group = groupDao.getGroup(id).orElseThrow(() ->
+              createHttpException(HttpStatus.NOT_FOUND, "Group not founded"));
       if (!group.getMemberList().containsKey(user)) {
-        throw new IllegalAccessException("User is not a part of group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User is not a part of group");
       }
       if (group.getMemberList().get(user).equals(UserRole.MEMBER)) {
-        throw new IllegalAccessException("User does not have enough rights to edit group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User does not have enough rights to edit group");
       }
       if ((group.getBaseGroup() == null && groupDto.getBaseGroupId() != 0)
               || groupDto.getBaseGroupId() != 0 && groupDto.getBaseGroupId() != group.getBaseGroup().getId()) {
         var newBaseGroup = groupDao.getGroup(groupDto.getBaseGroupId())
-                .orElseThrow(() -> new NoSuchElementException("Base group not founded"));
+                .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "Base group not founded"));
         if (!newBaseGroup.getMemberList().containsKey(user)) {
-          throw new IllegalAccessException("User is not a part of a base group");
+          throw createHttpException(HttpStatus.FORBIDDEN, "User is not a part of a base group");
         }
         if (newBaseGroup.getMemberList().get(user).equals(UserRole.MEMBER)) {
-          throw new IllegalAccessException("User does not have enough rights to create a subgroup");
+          throw createHttpException(HttpStatus.FORBIDDEN, "User does not have enough rights to create a subgroup");
         }
         group.setBaseGroup(newBaseGroup);
         group.setGroupType(GroupType.SUBGROUP);
@@ -139,15 +139,11 @@ public class GroupController {
       }
       group.setName(groupDto.getName());
       if (!groupDao.editGroup(group).equals(Statuses.SUCCESS)) {
-        throw new UnexpectedException("Something goes wrong");
+        throw createHttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Something goes wrong");
       }
       return ResponseEntity.ok(new GetGroupDto(group));
-    } catch (NoSuchElementException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    } catch (IllegalAccessException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
-    } catch (UnexpectedException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    } catch (HttpClientErrorException exception) {
+      return new ResponseEntity<>(exception.getStatusText(), exception.getStatusCode());
     }
   }
 
@@ -157,24 +153,21 @@ public class GroupController {
                                        @PathVariable long id) {
     try {
       var user = userDao.getUser(jwtTokenProvider.getUsername(token))
-              .orElseThrow(() -> new NoSuchElementException("user not founded"));
-      var group = groupDao.getGroup(id).orElseThrow(() -> new NoSuchElementException("Group not founded"));
+              .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "user not founded"));
+      var group = groupDao.getGroup(id).orElseThrow(() ->
+              createHttpException(HttpStatus.NOT_FOUND, "Group not founded"));
       if (!group.getMemberList().containsKey(user)) {
-        throw new IllegalAccessException("User is not a part of group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User is not a part of group");
       }
       if (!group.getMemberList().get(user).equals(UserRole.CREATOR)) {
-        throw new IllegalAccessException("User does not have enough rights to edit group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User does not have enough rights to edit group");
       }
       if (!groupDao.deleteGroup(id).equals(Statuses.SUCCESS)) {
-        throw new UnexpectedException("Something goes wrong");
+        throw createHttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Something goes wrong");
       }
       return ResponseEntity.ok().build();
-    } catch (NoSuchElementException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    } catch (IllegalAccessException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
-    } catch (UnexpectedException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    } catch (HttpClientErrorException exception) {
+      return new ResponseEntity<>(exception.getStatusText(), exception.getStatusCode());
     }
   }
 
--- src/main/java/ru/sfedu/finalqualifyingwork/rest/api/v1/GroupUserController.java
+++ src/main/java/ru/sfedu/finalqualifyingwork/rest/api/v1/GroupUserController.java
@@ -2,10 +2,12 @@ package ru.sfedu.finalqualifyingwork.rest.api.v1;
 
 import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import ru.sfedu.finalqualifyingwork.model.User;
 import ru.sfedu.finalqualifyingwork.model.enums.UserRole;
 import ru.sfedu.finalqualifyingwork.repository.interfaces.GroupDao;
@@ -15,10 +17,9 @@ import ru.sfedu.finalqualifyingwork.rest.api.v1.dto.user.PublicUserDto;
 import ru.sfedu.finalqualifyingwork.security.JwtTokenProvider;
 import ru.sfedu.finalqualifyingwork.util.Statuses;
 
-import java.rmi.UnexpectedException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
+
 
 @RestController
 @RequestMapping("api/v1/group")
@@ -29,6 +30,9 @@ public class GroupUserController {
   private final GroupDao groupDao;
   private final JwtTokenProvider jwtTokenProvider;
 
+  private HttpClientErrorException createHttpException(HttpStatus status, String message) {
+    return HttpClientErrorException.create(status, message, HttpHeaders.EMPTY, null, null);
+  }
 
   @GetMapping(path = "{id}/users")
   @PreAuthorize("hasAuthority('user:all')")
@@ -36,18 +40,17 @@ public class GroupUserController {
                                          @PathVariable long id) {
     try {
       var user = userDao.getUser(jwtTokenProvider.getUsername(token))
-              .orElseThrow(() -> new NoSuchElementException("user not founded"));
-      var group = groupDao.getGroup(id).orElseThrow(() -> new NoSuchElementException("group not founded"));
+              .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "user not founded"));
+      var group = groupDao.getGroup(id).orElseThrow(() ->
+              createHttpException(HttpStatus.NOT_FOUND, "group not founded"));
       if (!group.getMemberList().containsKey(user)) {
-        throw new IllegalAccessException("User is not a part of this group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User is not a part of this group");
       }
       Map<PublicUserDto, UserRole> publicUserMap = new HashMap<>();
       group.getMemberList().forEach((groupUser, userRole) -> publicUserMap.put(new PublicUserDto(groupUser), userRole));
       return ResponseEntity.ok(publicUserMap);
-    } catch (NoSuchElementException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    } catch (IllegalAccessException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
+    } catch (HttpClientErrorException exception) {
+      return new ResponseEntity<>(exception.getStatusText(), exception.getStatusCode());
     }
   }
 
@@ -58,22 +61,21 @@ public class GroupUserController {
                                         @PathVariable(name = "userId") long userId) {
     try {
       var user = userDao.getUser(jwtTokenProvider.getUsername(token))
-              .orElseThrow(() -> new NoSuchElementException("User not founded"));
-      var group = groupDao.getGroup(groupId).orElseThrow(() -> new NoSuchElementException("Group not founded"));
+              .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "User not founded"));
+      var group = groupDao.getGroup(groupId).orElseThrow(() ->
+              createHttpException(HttpStatus.NOT_FOUND, "Group not founded"));
       if (!group.getMemberList().containsKey(user)) {
-        throw new IllegalAccessException("User is not a part of this group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User is not a part of this group");
       }
       User requestUser = userDao.getUser(userId).orElseThrow(() ->
-              new NoSuchElementException("User you were looking for was not found"));
+              createHttpException(HttpStatus.NOT_FOUND, "User you were looking for was not found"));
       if (!group.getMemberList().containsKey(requestUser)) {
-        throw new IllegalAccessException("User you were looking for is not a part of this group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User you were looking for is not a part of this group");
       }
       return ResponseEntity.ok(new GroupUserDto(new PublicUserDto(requestUser),
               group.getMemberList().get(requestUser)));
-    } catch (NoSuchElementException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    } catch (IllegalAccessException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
+    } catch (HttpClientErrorException exception) {
+      return new ResponseEntity<>(exception.getStatusText(), exception.getStatusCode());
     }
   }
 
@@ -84,33 +86,28 @@ public class GroupUserController {
                                           @PathVariable(name = "userId") long userId) {
     try {
       var user = userDao.getUser(jwtTokenProvider.getUsername(token))
-              .orElseThrow(() -> new NoSuchElementException("User not founded"));
-      var group = groupDao.getGroup(groupId).orElseThrow(() -> new NoSuchElementException("Group not founded"));
+              .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "User not founded"));
+      var group = groupDao.getGroup(groupId).orElseThrow(() ->
+              createHttpException(HttpStatus.NOT_FOUND, "Group not founded"));
       if (!group.getMemberList().containsKey(user)) {
-        throw new IllegalAccessException("User is not a part of this group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User is not a part of this group");
       }
       if (group.getMemberList().get(user).equals(UserRole.MEMBER)) {
-        throw new IllegalAccessException("User does not have enough rights to create a subgroup");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User does not have enough rights to create a subgroup");
       }
       User requestUser = userDao.getUser(userId).orElseThrow(() ->
-              new NoSuchElementException("User you were looking for was not found"));
+              createHttpException(HttpStatus.NOT_FOUND, "User you were looking for was not found"));
       if (group.getMemberList().containsKey(requestUser)) {
-        throw new IllegalAccessException("User is already part of the group");
+        throw createHttpException(HttpStatus.CONFLICT, "User is already part of the group");
       }
       group.getMemberList().put(requestUser, UserRole.INVITED);
       if (!groupDao.editGroup(group).equals(Statuses.SUCCESS)) {
-        throw new UnexpectedException("Ошибка при обновлении информации о группе");
+        throw createHttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Ошибка при обновлении информации о группе");
       }
       return ResponseEntity.ok(new GroupUserDto(new PublicUserDto(requestUser),
               group.getMemberList().get(requestUser)));
-    } catch (NoSuchElementException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    } catch (IllegalAccessException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
-    } catch (UnexpectedException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    } catch (RuntimeException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    } catch (HttpClientErrorException exception) {
+      return new ResponseEntity<>(exception.getStatusText(), exception.getStatusCode());
     }
   }
 
@@ -122,35 +119,30 @@ public class GroupUserController {
                                            @PathVariable(name = "role") UserRole role) {
     try {
       var user = userDao.getUser(jwtTokenProvider.getUsername(token))
-              .orElseThrow(() -> new NoSuchElementException("User not founded"));
+              .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "User not founded"));
       var group = groupDao.getGroup(groupId).orElseThrow(() ->
-              new NoSuchElementException("Group not founded"));
+              createHttpException(HttpStatus.NOT_FOUND, "Group not founded"));
       if (!group.getMemberList().containsKey(user)) {
-        throw new IllegalAccessException("User is not a part of this group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User is not a part of this group");
       }
       if (group.getMemberList().get(user).equals(UserRole.MEMBER) ||
               group.getMemberList().get(user).equals(UserRole.INVITED)) {
-        throw new IllegalAccessException("User does not have enough rights to change users roles");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User does not have enough rights to change users roles");
       }
       User requestUser = userDao.getUser(userId).orElseThrow(() ->
-              new NoSuchElementException("User you were looking for was not found"));
+              createHttpException(HttpStatus.NOT_FOUND, "User you were looking for was not found"));
       if (role.equals(UserRole.CREATOR) || role.equals(UserRole.INVITED)) {
-        throw new RuntimeException("You cant change user status to " + role.toString().toLowerCase());
+        throw createHttpException(HttpStatus.FORBIDDEN,
+                "You cant change user status to " + role.toString().toLowerCase());
       }
       group.getMemberList().replace(requestUser, role);
       if (!groupDao.editGroup(group).equals(Statuses.SUCCESS)) {
-        throw new UnexpectedException("Ошибка при обновлении информации о группе");
+        throw createHttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Ошибка при обновлении информации о группе");
       }
       return ResponseEntity.ok(new GroupUserDto(new PublicUserDto(requestUser),
               group.getMemberList().get(requestUser)));
-    } catch (NoSuchElementException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    } catch (IllegalAccessException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
-    } catch (UnexpectedException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    } catch (RuntimeException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    } catch (HttpClientErrorException exception) {
+      return new ResponseEntity<>(exception.getStatusText(), exception.getStatusCode());
     }
   }
 
@@ -161,29 +153,26 @@ public class GroupUserController {
                                                @PathVariable(name = "userId") long userId) {
     try {
       var user = userDao.getUser(jwtTokenProvider.getUsername(token))
-              .orElseThrow(() -> new NoSuchElementException("User not founded"));
-      var group = groupDao.getGroup(groupId).orElseThrow(() -> new NoSuchElementException("Group not founded"));
+              .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "User not founded"));
+      var group = groupDao.getGroup(groupId).orElseThrow(() ->
+              createHttpException(HttpStatus.NOT_FOUND, "Group not founded"));
       if (!group.getMemberList().containsKey(user)) {
-        throw new IllegalAccessException("User is not a part of this group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User is not a part of this group");
       }
       if (group.getMemberList().get(user).equals(UserRole.MEMBER)) {
-        throw new IllegalAccessException("User does not have enough rights to create a subgroup");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User does not have enough rights to create a subgroup");
       }
       User requestUser = userDao.getUser(userId).orElseThrow(() ->
-              new NoSuchElementException("User you were looking for was not found"));
+              createHttpException(HttpStatus.NOT_FOUND, "User you were looking for was not found"));
       group.getMemberList().remove(requestUser);
       if (!groupDao.editGroup(group).equals(Statuses.SUCCESS)) {
-        throw new UnexpectedException("Ошибка при обновлении информации о группе");
+        throw createHttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Ошибка при обновлении информации о группе");
       }
       Map<PublicUserDto, UserRole> publicUserMap = new HashMap<>();
       group.getMemberList().forEach((groupUser, userRole) -> publicUserMap.put(new PublicUserDto(groupUser), userRole));
       return ResponseEntity.ok(publicUserMap);
-    } catch (NoSuchElementException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    } catch (IllegalAccessException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
-    } catch (UnexpectedException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    } catch (HttpClientErrorException exception) {
+      return new ResponseEntity<>(exception.getStatusText(), exception.getStatusCode());
     }
   }
 
@@ -193,29 +182,23 @@ public class GroupUserController {
                                             @PathVariable(name = "groupId") long groupId) {
     try {
       var user = userDao.getUser(jwtTokenProvider.getUsername(token))
-              .orElseThrow(() -> new NoSuchElementException("User not founded"));
+              .orElseThrow(() -> createHttpException(HttpStatus.NOT_FOUND, "User not founded"));
       var group = groupDao.getGroup(groupId).orElseThrow(() ->
-              new NoSuchElementException("Group not founded"));
+              createHttpException(HttpStatus.NOT_FOUND, "Group not founded"));
       if (!group.getMemberList().containsKey(user)) {
-        throw new IllegalAccessException("User is not a part of this group");
+        throw createHttpException(HttpStatus.FORBIDDEN, "User is not a part of this group");
       }
       if (!group.getMemberList().get(user).equals(UserRole.INVITED)) {
-        throw new RuntimeException("User is already accept invitation of the group");
+        throw createHttpException(HttpStatus.CONFLICT, "User is already accept invitation of the group");
       }
       group.getMemberList().replace(user, UserRole.MEMBER);
       if (!groupDao.editGroup(group).equals(Statuses.SUCCESS)) {
-        throw new UnexpectedException("Ошибка при обновлении информации о группе");
+        throw createHttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Ошибка при обновлении информации о группе");
       }
       return ResponseEntity.ok(new GroupUserDto(new PublicUserDto(user),
               group.getMemberList().get(user)));
-    } catch (NoSuchElementException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    } catch (IllegalAccessException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
-    } catch (UnexpectedException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    } catch (RuntimeException exception) {
-      return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    } catch (HttpClientErrorException exception) {
+      return new ResponseEntity<>(exception.getStatusText(), exception.getStatusCode());
     }
   }
 
